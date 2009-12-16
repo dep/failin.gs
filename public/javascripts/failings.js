28392ba21fc7bcecb04387475b57753f9723d32f
Danny Peck <dannypeck@gmail.com>
2009-12-16 10:56:14 -0500

    Add reply functionality, error messages. Comments

--- public/javascripts/failings.js
+++ public/javascripts/failings.js
@@ -8,3 +8,18 @@ function show_signup() {
   $('signup_form').show();
   $('username').focus();
 }
+
+function add_reply(e) {
+  e.style.display = "none";
+  var reply_html = "<textarea name='comment' id='comment'></textarea><input type='submit' value='Send Comment' />";
+  var pObj = e;
+  while (pObj.className != 'feedback')
+    pObj = pObj.parentNode;
+
+  var reply_div = document.createElement('div');
+  Element.extend(reply_div);
+  reply_div.addClassName('reply_wrapper');
+  reply_div.innerHTML = reply_html;
+  pObj.appendChild(reply_div);
+  $('comment').focus();
+}
