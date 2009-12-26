b9e5264d8649b555230cce42b0c448d8dd4796e0
Stephen Celis <stephen@stephencelis.com>
2009-12-26 11:26:00 -0500

    Connect emails model to user.

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,7 @@
 class Email < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :address
+
   def save
     super
   rescue ActiveRecord::RecordNotUnique => e
--- db/migrate/20091226152125_create_emails.rb
+++ db/migrate/20091226152125_create_emails.rb
@@ -1,11 +1,14 @@
 class CreateEmails < ActiveRecord::Migration
   def self.up
     create_table :emails do |t|
-      t.string :address, :invite_code
+      t.belongs_to :user
+      t.string :address, :null => false
+      t.string :invite_code
 
       t.timestamps
     end
 
+    add_index :emails, :user_id, :unique => true
     add_index :emails, :address, :unique => true
     add_index :emails, :invite_code
   end
