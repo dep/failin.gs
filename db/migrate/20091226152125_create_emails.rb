34542f7a99e4078c1c2bf5567be79ea3ccbc48b3
Stephen Celis <stephen@stephencelis.com>
2009-12-26 11:19:02 -0500

    Add invite code to email migration.

--- db/migrate/20091226152125_create_emails.rb
+++ db/migrate/20091226152125_create_emails.rb
@@ -1,12 +1,13 @@
 class CreateEmails < ActiveRecord::Migration
   def self.up
     create_table :emails do |t|
-      t.string :address
+      t.string :address, :invite_code
 
       t.timestamps
     end
 
     add_index :emails, :address, :unique => true
+    add_index :emails, :invite_code
   end
 
   def self.down
