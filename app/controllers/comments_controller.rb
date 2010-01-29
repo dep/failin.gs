f9be2f7e4b74d6d7ee880091143faa3ed027271e
Stephen Celis <stephen@stephencelis.com>
2010-01-28 22:35:45 -0600

    Add comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def create
+    @user = User.find_by_login! params[:login]
+    @failing = @user.failings.find params[:failing_id]
+    @comment = @failing.comments.new params[:comment]
+    @comment.user_id = current_user.id
+    @comment.submitter_ip = request.remote_ip
+
+    if @comment.save
+      redirect_to profile_url(@user)
+    else
+      render "failings#index"
+    end
+  end
 end
