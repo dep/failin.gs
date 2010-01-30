5592a359507ca6eb0a5d2d2e82cd6d2dc956f148
Stephen Celis <stephen@stephencelis.com>
2010-01-30 14:21:05 -0600

    Fix failings partial organization [fixes #17].

--- app/helpers/failings_helper.rb
+++ app/helpers/failings_helper.rb
@@ -1,6 +1,6 @@
 module FailingsHelper
-  def css_class_for(failing)
-    case failing.state
+  def css_class_for(state)
+    case state
       when "needs_review" then "unrated"
       when "knew"         then "knew"
       when "no_idea"      then "unknown"
@@ -8,8 +8,8 @@ module FailingsHelper
     end
   end
 
-  def title_for(failing)
-    case failing.state
+  def title_for(state)
+    case state
       when "needs_review" then "Needs review"
       when "knew"         then "I knew this about me"
       when "no_idea"      then "I had no idea"
