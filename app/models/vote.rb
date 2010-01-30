2ac21701a8202802bb836bc8c9dbe52c63ece614
Stephen Celis <stephen@stephencelis.com>
2010-01-30 17:00:46 -0600

    Drag-drop experiments.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,8 +3,8 @@ class Vote < ActiveRecord::Base
   belongs_to :failing
   belongs_to :user
   validates_presence_of :failing
-  validates_uniqueness_of :user_id, scope: :failing_id
-  validates_uniqueness_of :voter_ip, scope: :failing_id
+  validates_uniqueness_of :user_id, scope: [:failing_id, :voter_ip]
+  validates_uniqueness_of :voter_ip, scope: [:failing_id, :user_id]
   attr_protected :failing_id, :user_id, :voter_ip
 
   scope :positive, conditions: { agree: true }
