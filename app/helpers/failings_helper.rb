52e32a99c2ee0430990080d5706e4ac90f4c3320
Stephen Celis <stephen@stephencelis.com>
2010-01-31 16:46:01 -0600

    Add arrow logic.

--- app/helpers/failings_helper.rb
+++ app/helpers/failings_helper.rb
@@ -16,4 +16,30 @@ module FailingsHelper
       when "disagree"     then "I totally disagree"
     end
   end
+
+  def move_left_link(failing)
+    return if failing.state == "needs_review"
+
+    state = case failing.state
+      when "knew"     then "disagree"
+      when "no_idea"  then "knew"
+      when "disagree" then "no_idea"
+    end
+
+    link_to_remote image_tag("move_left.png", class: "left", title: "Move left one category", style: "display: none;"),
+      url: eval("#{state}_failing_path(failing.user, failing)"), method: :put
+  end
+
+  def move_right_link(failing)
+    return if failing.state == "needs_review"
+
+    state = case failing.state
+      when "knew"     then "no_idea"
+      when "no_idea"  then "disagree"
+      when "disagree" then "knew"
+    end
+
+    link_to_remote image_tag("move_right.png", class: "right", title: "Move right one category", style: "display: none;"),
+      url: eval("#{state}_failing_path(failing.user, failing)"), method: :put
+  end
 end
