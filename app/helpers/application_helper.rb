e3364049e357fb1867312b82a1a9f949485953eb
Stephen Celis <stephen@stephencelis.com>
2010-02-06 17:43:56 -0600

    Fix simple_format [fixes #39].

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
     return super if ActionController::Base.asset_host
     request.protocol + request.host_with_port + image_path(*args)
   end
+
+  def simple_format(text, html_options={})
+    start_tag = tag('p', html_options, true)
+    text = h(text)
+    text.gsub!(/^\s*/, start_tag)
+    text.gsub!(/\s*$/, '</p>')
+    text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n
+    text.gsub!(/\n\n+/, "</p>\n\n#{start_tag}")  # 2+ newline  -> paragraph
+    text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> br
+    text
+  end
 end
