1310f45cd73f1911b2fea4f01505cf9444cb88c4
Stephen Celis <stephen@stephencelis.com>
2010-02-13 12:54:30 -0600

    Seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+user = User.new login: "failings",
+                email: "app@failin.gs",
+             password: "password",
+              surname: "Gs"
+
+user.invites_left = 5000
+user.save validate: false
