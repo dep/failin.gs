15c63355ccbad23b30f2d803fdf6fcd5ade99edd
Stephen Celis <stephen@stephencelis.com>
2011-08-07 10:39:50 -0700

    Don't verify SSL.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.configuration.middleware.use OmniAuth::Builder do
-  provider :facebook, App.facebook[:key], App.facebook[:secret]
-  provider :twitter,  App.twitter[:key],  App.twitter[:secret]
+  options = { client_options: { ssl: { ca_path: "/etc/ssl/certs" } } }
+  provider :facebook, App.facebook[:key], App.facebook[:secret], options
+  provider :twitter,  App.twitter[:key],  App.twitter[:secret],  options
 end
